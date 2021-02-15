1547f103a24f5636f61634521224b76040ab8ecd
Adam-Henrie <adamhenriem@gmail.com>
2021-02-15 12:17:36 -0500

    First Commit

---
+++ app/src/main/java/com/example/buttonclicker/LoginRegisterActivity.java
@@ -0,0 +1,94 @@
+package com.example.buttonclicker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+
+
+    private static final String TAG = "LoginRegisterActivity";
+        int AUTHUI_REQUEST_CODE = 10001;
+
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+
+
+        if (FirebaseAuth.getInstance().getCurrentUser()  != null    ){
+            startActivity(new Intent(this, MainActivity.class));
+            this.finish();
+        }
+
+    }
+
+
+
+
+
+    public void handleLoginRegister(View view){
+
+        List<AuthUI.IdpConfig> provider = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                .setTosAndPrivacyPolicyUrls("https://example.com", "https://example.com")
+                .setLogo(R.drawable.common_google_signin_btn_icon_light_normal_background)
+                .setAlwaysShowSignInMethodScreen(true)
+                .build();
+
+        startActivityForResult(intent, AUTHUI_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AUTHUI_REQUEST_CODE){
+            if(resultCode == RESULT_OK) {
+                // We have signed in the user or we have a new user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: " + user.getEmail());
+
+                //this pulls the meta data for the user to check to see if they are a new user or not.
+                // we can use this to pass the user name info on to the main activity to make a document reference to make a new Firestore
+                // database entry.
+                if (user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()){
+                    Toast.makeText(this, "Welcome New User" + user.getDisplayName().toString() + " ", Toast.LENGTH_SHORT).show();
+                }
+
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                this.finish();
+
+            } else {
+                //Signing in failed
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if (response == null) {
+                    Log.d(TAG, "onActivityResult: the user has cancelled the sign in request ");
+                }else{
+                    Log.e(TAG, "onActivityResult: " , response.getError());
+                }
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/buttonclicker/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.buttonclicker;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String Quote_Key = "quote";
+    public static final String Author_Key = "author";
+
+    TextView mQuoteTextView;
+
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
+
+    private DocumentReference rDocRef = FirebaseFirestore.getInstance().document("sampleData/with_more_inspiration");
+
+    Button btn_button;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuoteTextView = (TextView) findViewById(R.id.quote_display);
+
+        btn_button = findViewById(R.id.btn_button);
+        EditText tv_quote = findViewById(R.id.et_quote);
+        EditText tv_author =  findViewById(R.id.et_author);
+
+
+        //if user hasn't logged in before create an intent which transfers you to the LoginRegisterActivity
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startLoginActivity();
+        }
+
+    }
+
+    public void startLoginActivity(){
+        Intent intent = new Intent(this, LoginRegisterActivity.class);
+        startActivity(intent);
+        this.finish();
+    };
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                AuthUI.getInstance().signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    startLoginActivity();
+                                } else {
+                                    System.out.println("Failed");
+                                }
+                            }
+                        });
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public void fetchQuote(View view){
+        mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()){
+                    String quoteText = documentSnapshot.getString(Quote_Key);
+                    String authorText = documentSnapshot.getString(Author_Key);
+                    mQuoteTextView.setText("\"" + quoteText + "\" -- " + authorText);
+                }
+            }
+        });
+
+    }
+
+
+
+    public void saveQuote(View view){
+       EditText quoteView =   findViewById(R.id.et_quote);
+        EditText authorView =  findViewById(R.id.et_author);
+        String quoteText = quoteView.getText().toString();
+        String authorText = authorView.getText().toString();
+
+
+        if (quoteText.isEmpty() || authorText.isEmpty()) {
+            return;
+        }
+        Map<String, Object> dataToSave = new HashMap<String, Object>();
+        dataToSave.put(Quote_Key, quoteText);
+        dataToSave.put(Author_Key, authorText);
+        Map<String, Object> dataToAdd  = new HashMap<String, Object>();
+        dataToAdd.put("qussss", quoteText);
+        dataToAdd.put("asds", authorText);
+
+        mDocRef.update(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("InspiringQuote", "Document has been saved!");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w("InspiringQuote", "Document was not saved!", e);
+            }
+        });
+
+
+        rDocRef.set(dataToSave);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
